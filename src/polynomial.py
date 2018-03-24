c7b664a2074b6934109b1a8911de16f763ea87cd
David Lambropoulos <d.a.lambropoulos91@gmail.com>
2018-03-24 14:45:33 -0400

    Brute force O(n^2) polynomial multiply added.

--- src/polynomial.py
+++ src/polynomial.py
@@ -1,3 +1,28 @@
-def polynomial(object):
+class polynomial(object):
     def __init__(self):
         self.x = 0
+
+    # A - array of m elements
+    # B - array of n elements
+    def bruteForceMultiply(self, A, B):
+        # Intialize C of size m+n-1
+        C = []
+        i = 0
+        stop = len(A)+len(B)-1
+        
+        # O(m+n)
+        while i < stop:
+            C.append(0)
+            i += 1
+
+        # O(n^2)
+        i = 0
+        for a in A:
+            j = 0
+            for b in B:
+                C[i+j] += a*b
+                j += 1
+            i += 1
+
+        print(C)
+        return C
